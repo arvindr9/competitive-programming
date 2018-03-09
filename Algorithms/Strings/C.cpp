5ae90b83c08113f4d9c3f48a94db35a1ae867ae5
Arvind Ramaswami <saramaswami@gmail.com>
2018-03-09 14:57:06 -0500

    Update C.cpp

--- Algorithms/Strings/C.cpp
+++ Algorithms/Strings/C.cpp
@@ -1,6 +1,7 @@
 /*
     Codeforces Problem 514C: Watto and Mechanism (http://codeforces.com/problemset/problem/514/C)
     Author: arvindr9
+    Hash of a string A_0 A_1 ... A_k is (A_0 * x^k + A_1 * x^{k - 1} + ... + A_k * x^0) mod p
 */
 
 #include <bits/stdc++.h>
@@ -9,13 +10,12 @@ using namespace std;
 typedef long long ll;
 const int maxk = 1000000; //max value of a string's length
 int n, m;
-int slen = 0; //the length of the string that was previously processed: used to efficiently zero out the dp arrays
 const ll p = 1000000007;
 const ll x = 42863052;
 const ll x2 = 47825835;
-set<ll> set1;
-map<ll, set<char>> mapc; 
-map<ll, set<int>> mapi;
+set<ll> set1; //stores hashcodes
+map<ll, set<char>> mapc; //stores the characters that are zeroed to generate each hashcode
+map<ll, set<int>> mapi; //stores the indexes that are zeroed to generate each hashcode
 set<ll> set2;
 map<ll, set<char>> mapc2;
 map<ll, set<int>> mapi2;
@@ -62,10 +62,10 @@ void checkValidity(string s) {
     for(int i = 0; i < len; i++) {
         ll hash = (h - ((s[i] - '0') * poww[len - i - 1]) % p + p) % p;
         ll hash2 = (h2 - ((s[i] - '0') * poww2[len - i - 1]) % p + p) % p;
-        unsigned int req_size = 1 + (mapc[hash].count(s[i]) ? 1 : 0);
+        unsigned int req_size = 1 + (mapc[hash].count(s[i]) ? 1 : 0); //the minimum number of distinct characters that should be zeroed to generate the hashcode (this is equal to 2 if there is a memory string that is equal to the query string and 1 otherwise). 
         unsigned int req_size2 = 1 + (mapc2[hash2].count(s[i]) ? 1 : 0);
         if(set1.count(hash) && (mapc[hash].size() >= req_size) && mapi[hash].count(i)
-            && set2.count(hash2) && (mapc2[hash2].size() >= req_size2) && mapi2[hash2].count(i)) { //hash already exists, different characters were used to hash, hashes were done at the same indices. Issue: there exists another string that produces the same hash value when an index is removed.
+            && set2.count(hash2) && (mapc2[hash2].size() >= req_size2) && mapi2[hash2].count(i)) {
             cout << "YES\n";
             return;
         } 
@@ -74,8 +74,8 @@ void checkValidity(string s) {
 }
 
 int main() {
-    freopen("C.in", "r", stdin);
-    freopen("C.out", "w", stdout);
+    //freopen("C.in", "r", stdin);
+    //freopen("C.out", "w", stdout);
     ios::sync_with_stdio(0);
     cin.tie(0);
     cin >> n >> m;
