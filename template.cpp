381af09d8e80e0809db3fb1bb381efbb011bfa20
Arvind Ramaswami <saramaswami@gmail.com>
2018-03-10 20:59:58 -0800

    Update template.cpp

--- template.cpp
+++ template.cpp
@@ -3,6 +3,21 @@
 using namespace std;
 
 typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+
+#define mp make_pair
+#define pb push_back
+#define f first
+#define s second
+#define lb lower_bound
+#define ub upper_bound
+
+const int MOD = 1000000007;
+const double PI = 4*atan(1);
+const ll INF = 1e18;
+const int MX = 100001;
+
 
 int main() {
     ios::sync_with_stdio(0);
