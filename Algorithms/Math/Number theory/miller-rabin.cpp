3b4878bacd40f22d07e96ac57e7fb95df577f510
Arvind Ramaswami <saramaswami@gmail.com>
2018-03-21 10:40:38 -0700

    Update miller-rabin.cpp

--- Algorithms/Math/Number theory/miller-rabin.cpp
+++ Algorithms/Math/Number theory/miller-rabin.cpp
@@ -9,15 +9,22 @@ ll modpow(ll a, ll power, int mod) {
 }
 
 void createA() {
-    a.push_back(2);
-    a.push_back(3);
-    a.push_back(5);
-    a.push_back(7);
-    //possibly up till 37-ish
+    as.push_back(2);
+    as.push_back(3);
+    as.push_back(5);
+    as.push_back(7);
+    as.push_back(11);
+    as.push_back(13);
+    as.push_back(17);
+    as.push_back(23);
+    as.push_back(29);
+    as.push_back(31);
+    as.push_back(37);
 }
 
-bool isPrime(int n) {
-    int d = n - 1;
+bool isPrime(ll n) {
+    if (n % 2 == 0) return false;
+    ll d = n - 1;
     int s = 0;
     while (d % 2 == 0) {
         d /= 2;
@@ -26,12 +33,20 @@ bool isPrime(int n) {
     bool isPrime = true;
     for(int a: as) {
         ll test = (modpow(a, d, n) + n) % n;
-        if (test == 1) continue;
-        for (int i = 0; i < s; i++) {
+        if (a == n) continue;
+        if (test == 1LL) continue;
+        if (test == n - 1) continue;
+        if (s == 1) {
+            isPrime = false;
+        }
+        for (int i = 1; i < s; i++) {
             test = (test * test) % n;
             if (test == n - 1) break;
-            if (i == s - 1) isPrime = false;
+            if (i == s - 1) {
+                isPrime = false;
+            }
         }
     }
     return isPrime;
 }
+
