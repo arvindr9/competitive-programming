3b92fb9a11523a4ba73e21a4307cc9a9862e3f27
Arvind Ramaswami <saramaswami@gmail.com>
2020-01-14 05:27:26 -0500

    Update template.cpp

--- template.cpp
+++ template.cpp
@@ -7,18 +7,12 @@ using namespace __gnu_pbds;
 
 typedef long long ll;
 typedef long double ld;
-typedef vector<int> vi;
 typedef pair<int, int> pi;
-typedef vector<pi> vpi;
 typedef pair<ll,ll> pl;
 typedef pair<double,double> pd;
+typedef priority_queue<int, vector<int>, greater<int>> min_heap;
 
 template <class T> using Tree = tree<T, null_type, less<T>, rb_tree_tag,tree_order_statistics_node_update>;
-
-#define FOR(i, a, b) for (int i=a; i<(b); i++)
-#define F0R(i, a) for (int i=0; i<(a); i++)
-#define FORd(i,a,b) for (int i = (b)-1; i >= a; i--)
-#define F0Rd(i,a) for (int i = (a)-1; i >= 0; i--)
 
 #define sz(x) (int)(x).size()
 #define mp make_pair
@@ -29,7 +23,6 @@ template <class T> using Tree = tree<T, null_type, less<T>, rb_tree_tag,tree_ord
 #define ub upper_bound
 #define all(x) x.begin(), x.end()
 
-const int MOD = 1000000007;
 const double PI = 4*atan(1);
 const ll INF = 1e18;
 const int MX = 100001;
