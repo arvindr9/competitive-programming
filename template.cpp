b7fe873629228420c7722621383b2e2d5731babd
Arvind Ramaswami <saramaswami@gmail.com>
2020-01-14 09:29:44 -0500

    Update template.cpp

--- template.cpp
+++ template.cpp
@@ -1,6 +1,6 @@
 #include <bits/stdc++.h>
-#include <ext/pb_ds/tree_policy.hpp>
-#include <ext/pb_ds/assoc_container.hpp>
+// #include <ext/pb_ds/tree_policy.hpp>
+// #include <ext/pb_ds/assoc_container.hpp>
 
 using namespace std;
 using namespace __gnu_pbds;
@@ -12,9 +12,8 @@ typedef pair<ll,ll> pl;
 typedef pair<double,double> pd;
 typedef priority_queue<int, vector<int>, greater<int>> min_heap;
 
-template <class T> using Tree = tree<T, null_type, less<T>, rb_tree_tag,tree_order_statistics_node_update>;
+// template <class T> using Tree = tree<T, null_type, less<T>, rb_tree_tag,tree_order_statistics_node_update>;
 
-#define sz(x) (int)(x).size()
 #define mp make_pair
 #define pb push_back
 #define f first
