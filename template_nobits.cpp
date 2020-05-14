a23c1993a61690a3d599ece3a62f6fb781edd4f2
Arvind Ramaswami <saramaswami@gmail.com>
2020-05-14 09:54:16 -0400

    Create template_nobits.cpp

---
+++ template_nobits.cpp
@@ -0,0 +1,42 @@
+#include <vector>
+#include <set>
+#include <map>
+#include <queue>
+#include <stack>
+#include <deque>
+
+#include <algorithm>
+
+// #include <bits/stdc++.h>
+// #include <ext/pb_ds/tree_policy.hpp>
+// #include <ext/pb_ds/assoc_container.hpp>
+
+using namespace std;
+// using namespace __gnu_pbds;
+
+typedef long long ll;
+typedef long double ld;
+typedef pair<int, int> pi;
+typedef pair<ll,ll> pl;
+typedef pair<double,double> pd;
+typedef priority_queue<int, vector<int>, greater<int>> min_heap;
+
+// template <class T> using Tree = tree<T, null_type, less<T>, rb_tree_tag,tree_order_statistics_node_update>;
+
+#define mp make_pair
+#define pb push_back
+#define f first
+#define s second
+#define lb lower_bound
+#define ub upper_bound
+#define all(x) x.begin(), x.end()
+
+const double PI = 4*atan(1);
+const ll INF = 1e18;
+const int MX = 100001;
+
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+}
